dd2aaf91c439d99c23632cd62488a9f9e458ad88
Jayakranth <jayakranthreddy@gmail.com>
2021-11-18 23:29:16 +0100

    dfjhk

--- my_cpp_pkg/src/number_counter.cpp
+++ my_cpp_pkg/src/number_counter.cpp
@@ -1,5 +1,9 @@
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/msg/int64.hpp"
+#include "example_interfaces/srv/set_bool.hpp"
+using std::placeholders::_1;
+using std::placeholders::_2;
+
 class NumberCounter : public rclcpp::Node
 {
 public:
@@ -10,6 +14,9 @@ public:
         subscriber_ = this->create_subscription<example_interfaces::msg::Int64>(
             "number", 10,
             std::bind(&NumberCounter::callbackNews, this, std::placeholders::_1));
+            server_ = this->create_service<example_interfaces::srv::SetBool>("reset_counter",
+                                                                         std::bind(&NumberCounter::callBackResetCounter, this, _1, _2));
+
         RCLCPP_INFO(this->get_logger(), "number_counter started. ");
     }
 
@@ -20,13 +27,28 @@ private:
         counter_ += msg->data;
         auto new_msg= example_interfaces::msg::Int64();
         new_msg.data=counter_;
-        counter_publisher->publish(new_msg);
-
-        
+        counter_publisher->publish(new_msg);        
+    }
+    void callBackResetCounter(const example_interfaces::srv::SetBool::Request::SharedPtr request,
+                              const example_interfaces::srv::SetBool::Response::SharedPtr response)
+    {
+        if (request->data)
+        {
+            counter_ = 0;
+            response->success = true;
+            response->message = "counter has been reset";
+        }
+        else
+        {
+            response->success = false;
+            response->message = "counter has not been reset";
+        }
     }
     int counter_;
     rclcpp::Subscription<example_interfaces::msg::Int64>::SharedPtr subscriber_;
     rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr counter_publisher;
+    rclcpp::Service<example_interfaces::srv::SetBool>::SharedPtr server_;
+
 };
 int main(int argc, char **argv)
 {
